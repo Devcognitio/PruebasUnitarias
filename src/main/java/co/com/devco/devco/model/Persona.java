caef7f830f98e9b27a84f68715f43b0f887b8653
jbeltranc <jbeltranc@devco.com.co>
2018-11-23 08:27:31 -0500

    |Pruebas Unitarias|
    Se adiciona arquetipo inicial para pruebas unitarias avanzadas

---
+++ src/main/java/co/com/devco/devco/model/Persona.java
@@ -0,0 +1,32 @@
+package co.com.devco.devco.model;
+
+public class Persona {
+
+    private String nombre;
+    private String apelido;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
